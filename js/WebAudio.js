4398489dab2088d0317edcf752f555719cbc67ee
quyans@sina.com <quyans@sina.com>
2017-07-22 15:37:53 +0800

    创建

---
+++ js/WebAudio.js
@@ -0,0 +1,106 @@
+/**
+ * Created by 延松松松松 on 2017/7/22.
+ */
+var context = new AudioContext();
+var source = null;
+var audioBuffer = null;
+var PresentSong;
+
+
+function stopSound() {
+    if (source) {
+        source.stop(0); //立即停止
+    }
+}
+function playSound() {
+    source = context.createBufferSource();
+    source.buffer = audioBuffer;
+    source.loop = true;
+    source.connect(context.destination);
+    source.start(0); //立即播放
+}
+function initSound(arrayBuffer) {
+    context.decodeAudioData(arrayBuffer, function(buffer) { //解码成功时的回调函数
+        audioBuffer = buffer;
+        playSound();
+    }, function(e) { //解码出错时的回调函数
+        console.log('Error decoding file', e);
+    });
+}
+function loadAudioFile(url) {
+    var xhr = new XMLHttpRequest(); //通过XHR下载音频文件
+    xhr.open('GET', url, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = function(e) { //下载完成
+        initSound(this.response);
+    };
+    xhr.send();
+}
+// loadAudioFile("music/0.mp3");
+
+//*************************************
+// for(var i = 0;i<showButton_Act.length;i++){
+//     showButton_Act[i].addEventListener("click",(function () {
+//         console.log(123)
+//         var index = i;
+//
+//
+// // console
+//         return function () {
+//
+//             // console.log("我检查到了监听器")
+//             // getGroupNumber(index+1);
+//             // getGroupIntro(index+1);
+//             console.log(index);
+//
+//             var Intro_title = data.data.activities[index].id;
+//             var Intro_Content = data.data.activities[index].introduce;
+//             showActDetailIntro(Intro_title,Intro_Content);
+//         }
+//
+//
+//     })(i)) ;
+//     showPage(ActpageInt,ActpageMax);
+// }
+//********************************************************************************
+
+
+    var SongName = document.getElementsByClassName("SongName");
+    for(var i = 0; i<SongName.length ; i++)
+    {
+        SongName[i].addEventListener("click",(function () {
+
+             var index = i;
+
+             return function () {
+                 console.log("我检测到了监听器" + index);
+                 stopSound();
+                 loadAudioFile("music/"+index+".mp3");
+                 PresentSong = index
+             }
+
+        })(i));
+    }
+
+
+    function preSong(presentIndex) {
+
+        if(presentIndex>=1){
+            document.getElementById("preButton").onclick = function () {
+                console.log(presentIndex)
+                stopSound();
+                loadAudioFile("music/"+presentIndex-1+".mp3");
+        }
+
+        }
+    }
+
+    function nextSong(presentIndex) {
+        if(presentIndex<=2){
+            document.getElementById("nextButton").onclick = function () {
+                stopSound();
+                loadAudioFile("music/"+presentIndex+1+".mp3");
+            }
+
+        }
+    }
